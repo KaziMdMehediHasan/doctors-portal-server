19ab897f52c2ed1605b43eb78dad35a8943ab397
= <=>
2021-11-19 12:56:53 +0600

    added admin service account

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 
 
 // JWT token config middleware
-const serviceAccount = require("./doctors-portal-450bf-firebase-adminsdk-1qkku-32fb51c3d0(1).json");
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT)
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
